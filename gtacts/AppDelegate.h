67b6cd6f44575035eb031479b502364811882bf4
Chris <4f428288@opayq.com>
2016-02-28 17:51:14 -0800

    Created Xcode project

---
+++ gtacts/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gtacts
+//
+//  Created by Chris on 2/28/16.
+//  Copyright © 2016 csjones. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gtacts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gtacts
+//
+//  Created by Chris on 2/28/16.
+//  Copyright © 2016 csjones. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
