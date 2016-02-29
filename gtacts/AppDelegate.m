f591507a980adcf65b0b836a9d8e9f395eb3ffba
Chris <4f428288@opayq.com>
2016-02-28 19:56:34 -0800

    Changed to deprecated method

--- gtacts/AppDelegate.m
+++ gtacts/AppDelegate.m
@@ -24,9 +24,10 @@
     return YES;
 }
 
-- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary *)options {
-    return [[GIDSignIn sharedInstance] handleURL:url sourceApplication:nil annotation:nil];
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+    return [[GIDSignIn sharedInstance] handleURL:url sourceApplication:sourceApplication annotation:annotation];
 }
+
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
