30e96e08a8801fd725407aa2cf89190851ef079f
Chris <4f428288@opayq.com>
2016-02-29 17:49:30 -0800

    Added Google Contacts model

---
+++ gtacts/GoogleUserContacts.h
@@ -0,0 +1,13 @@
+//
+//  GoogleUserContacts.h
+//  gtacts
+//
+//  Created by Chris on 2/28/16.
+//  Copyright © 2016 csjones. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GoogleUserContacts : NSObject
+
+@end
